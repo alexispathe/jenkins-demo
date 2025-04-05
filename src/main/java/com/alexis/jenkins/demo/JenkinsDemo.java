f97b2a87531fe76dd33ef917ab76797df0b1b459
AlexPruebas <apathe26@alumnos.uaq.mx>
2025-04-05 15:34:11 -0600

    Proyecto

---
+++ src/main/java/com/alexis/jenkins/demo/JenkinsDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.alexis.jenkins.demo;
+
+/**
+ *
+ * @author Alexis
+ */
+public class JenkinsDemo {
+
+    public static void main(String[] args) {
+      System.out.println("Hola desde Jenkins y Maven! 🚀");
+    }
+}
